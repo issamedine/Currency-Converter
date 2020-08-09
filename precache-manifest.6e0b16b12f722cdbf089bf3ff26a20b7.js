a43110c60a575250903136eaeca31632bbce9c08
Issam <issam.b@tekru.net>
2020-08-09 12:57:17 +0200

    Updates

---
+++ precache-manifest.6e0b16b12f722cdbf089bf3ff26a20b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc75e27a27eee4137d0059a34f2c394",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "84e51c77736c06cea391",
+    "url": "/currency-converter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c4c1f007c9684617974",
+    "url": "/currency-converter/static/js/2.d72c04fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter/static/js/2.d72c04fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e51c77736c06cea391",
+    "url": "/currency-converter/static/js/main.4a980d0a.chunk.js"
+  },
+  {
+    "revision": "a0730edf8381b13d6dc3",
+    "url": "/currency-converter/static/js/runtime-main.46121f8e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/currency-converter/static/media/logo.5d5d9eef.svg"
+  }
+]);
